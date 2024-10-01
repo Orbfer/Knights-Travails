8cd163cb64090a6beb4e5c73ea85f651a78b8802
Orbfer <misha9043@gmail.com>
2024-10-01 20:39:14 +0300

    Finished function for knight.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./styles/styles.css";
 import "animate.css";
+import getKnightsRoute from "./scripts/getKnightsRoute";
+getKnightsRoute([1, 2], [3, 7]);
---
+++ src/scripts/getKnightsRoute.js
@@ -0,0 +1,65 @@
+let visitedSquares = [];
+function getKnightsRoute(start, end) {
+  visitedSquares = [];
+  if (!isValid(start) || !isValid(end)) {
+    console.error("Invalid start or end position");
+    return;
+  }
+
+  const queue = [[start]];
+
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const currentPosition = path[path.length - 1];
+
+    if (currentPosition[0] === end[0] && currentPosition[1] === end[1]) {
+      console.log("Shortest path:", path);
+      console.log("Number of steps:", path.length - 1);
+      return path;
+    }
+
+    const possibleMoves = methods(currentPosition[0], currentPosition[1]);
+
+    for (const move of possibleMoves) {
+      if (isValid(move)) {
+        visitedSquares.push(move.toString());
+        queue.push([...path, move]);
+      }
+    }
+  }
+
+  console.error("No path found.");
+}
+
+function isValid([x, y]) {
+  return (
+    x >= 0 &&
+    x < 8 &&
+    y >= 0 &&
+    y < 8 &&
+    !visitedSquares.includes([x, y].toString())
+  );
+}
+
+function methods(x, y) {
+  const moves = [
+    [x + 1, y + 2],
+    [x + 1, y - 2],
+    [x - 1, y + 2],
+    [x - 1, y - 2],
+    [x + 2, y + 1],
+    [x + 2, y - 1],
+    [x - 2, y + 1],
+    [x - 2, y - 1],
+  ];
+
+  const validMoves = [];
+  for (const move of moves) {
+    if (isValid(move)) {
+      validMoves.push(move);
+    }
+  }
+  return validMoves;
+}
+
+export default getKnightsRoute;
