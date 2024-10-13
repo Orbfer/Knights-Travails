9eaa3b3839236aad608d468764979af69bbb25ff
Orbfer <misha9043@gmail.com>
2024-10-13 15:36:40 +0300

    Started working on the animations for the knight.

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -6,6 +6,28 @@ function startMoves() {
     alert("You have to pick a start and an end point.");
   else {
     const path = getKnightsRoute(knightPos, endPoint);
+    for (let i = 1; i < path.length; i++) {
+      moveAnimation(path[i - 1], path[i]);
+    }
+  }
+}
+function moveAnimation(start, end) {
+  const knight = document.querySelector("#knight-icon");
+  if (start[0] + 1 === end[0] && start[1] + 2 === end[1]) {
+  }
+  if (start[0] + 1 === end[0] && start[1] - 2 === end[1]) {
+  }
+  if (start[0] - 1 === end[0] && start[1] + 2 === end[1]) {
+  }
+  if (start[0] - 1 === end[0] && start[1] - 2 === end[1]) {
+  }
+  if (start[0] + 2 === end[0] && start[1] + 1 === end[1]) {
+  }
+  if (start[0] + 2 === end[0] && start[1] - 1 === end[1]) {
+  }
+  if (start[0] - 2 === end[0] && start[1] + 1 === end[1]) {
+  }
+  if (start[0] - 2 === end[0] && start[1] - 1 === end[1]) {
   }
 }
 export default startMoves;
